89ed321417056e165333c54600df23979feb1ef5
1c3fr34k <56366285+1c3fr34k@users.noreply.github.com>
2023-07-19 17:01:11 +0200

    added some tests

---
+++ sortfs/sortfs_test.go
@@ -0,0 +1,25 @@
+package sortfs
+
+import (
+	"testing"
+)
+
+func TestGetFolderAndFilePathsLenFiles(t *testing.T) {
+	files, _ := getFolderAndFilePaths("C:\\Users\\manue\\Desktop\\DEV\\Go\\sortFS\\Test\\Unittest")
+	if len(files) != 1 {
+		t.Errorf("Expected 2 files, got %d", len(files))
+	}
+}
+
+func TestGetFolderAndFilePathsLenFolders(t *testing.T) {
+	_, folders := getFolderAndFilePaths("C:\\Users\\manue\\Desktop\\DEV\\Go\\sortFS\\Test\\Unittest")
+	if len(folders) != 1 {
+		t.Errorf("Expected 1 folder, got %d", len(folders))
+	}
+}
+
+func BenchmarkGetFolderAndFiles(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		getFolderAndFilePaths("C:\\Users\\manue\\Desktop\\DEV\\Go\\sortFS\\Test\\Unittest")
+	}
+}
