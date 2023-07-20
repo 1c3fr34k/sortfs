df9e6326dd3d3869a24afbc92fe179d651daa8ee
1c3fr34k <56366285+1c3fr34k@users.noreply.github.com>
2023-07-21 00:06:26 +0200

    cleanup

--- main.go
+++ main.go
@@ -1,19 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"time"
-
 	"github.com/1c3fr34k/sortFS/sortfs"
 )
 
 func main() {
-	start := time.Now()
 
-	rootpath := sortfs.New("C:\\Users\\manue\\Downloads\\Test\\Test")
-	rootpath.Sort()
-
-	elapsed := time.Since(start)
-
-	fmt.Printf("\nElapsed time: %s", elapsed)
+	sortfs.CLI()
 }
